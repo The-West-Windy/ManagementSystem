e039afc69e02758565fde20a4a0c0bb463025024
Max Honchar <maxim20051228@gmail.com>
2025-11-03 22:02:27 +0200

    Setup ASP.NET Core with EF Core

--- Server/ServerApp/Program.cs
+++ Server/ServerApp/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using ServerApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
