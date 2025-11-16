e6469861d2bcb13328bf24f5acae04d43d4cb3bf
Max Honchar <maxim20051228@gmail.com>
2025-11-16 19:37:53 +0200

    Integrate MAUI with REST API

--- Client/ClientApp/Models/BorrowRequest.cs
+++ Client/ClientApp/Models/BorrowRequest.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ClientApp.Models
 {
     public class BorrowRequest
     {
-        public string BookTitle { get; set; } = string.Empty;
-        public string BorrowerName { get; set; } = string.Empty;
-        public DateTime RequestedOn { get; set; } = DateTime.UtcNow;
-        public string Notes { get; set; } = string.Empty;
+        public int LibrarianID { get; set; }
+        public int BookID { get; set; }
+        public string Status { get; set; } = "Pending";
     }
 }
